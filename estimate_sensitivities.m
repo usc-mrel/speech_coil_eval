b820e37570bc68b8c1fa2c170c8221e8051b553f
Felix <47433452+felixmri@users.noreply.github.com>
2021-08-17 23:32:47 -0700

    Edited Comments

--- estimate_sensitivities.m
+++ estimate_sensitivities.m
@@ -17,7 +17,7 @@ N_slices = size(k,3);
 % Initialize zero matrix:
 cal = zeros(size(k));
 
-%% crop k space:
+%% crop k space to extract center lines:
 
 % Get center indices:
 
@@ -27,15 +27,15 @@ indices{i} = (1:extent)+bitshift(size(k, dim)-extent+1,-1);
 end
 
 
-% Extract indices:
+% Extract center indices:
 crop_k = k(indices{1},indices{2},indices{3},:);
 
-% Smooth kspace;
+% Smooth kspace with a 3D hamming window:
 filter = hamming_3d(size(crop_k,1),size(crop_k,2),size(crop_k,3));
 filter = repmat(filter,[1,1,1,size(crop_k,4)]);
 crop_k   = crop_k .* filter; 
 
-% Insert into zero matrix:
+% Insert into zero matrix for zero padding:
 cal(indices{1},indices{2},indices{3},:) = crop_k; 
 
 %% Get calibration images:
@@ -43,7 +43,7 @@ cal(indices{1},indices{2},indices{3},:) = crop_k;
 cal_images = ismrm_transform_kspace_to_image(cal,[1,2,3]); 
 
 
-%% Get sensitivity maps:
+%% Get Walsh sensitivity maps:
 
 csm = zeros(size(cal_images));
 
--- snr_recon_3d.m
+++ snr_recon_3d.m
@@ -1,8 +1,10 @@
-%addpath(genpath('/Users/felix/Research_Local'))
 
 % Script to perform SNR unit reconstruction on 3D imaging and noise data.
 % Takes in imaging data and noise data, and ouputs SNR recon in RSS and B1
 % Weighted. 
+
+% The data in this demo is located in: 
+% speech_coil_eval/raw_data/Vol_52/GRE/Speech_Body
 
 
 
@@ -14,26 +16,23 @@ data_filename = 'Speech_GRE.h5';
 flag.concatenate = 0;
 [kdata,~] = read_h5_data(data_filename,flag);
 
-% This is the filename of noise data
-% Read noise and output decorrelation matrix:
-%noise_filename = 'noise_from_speech_data.h5';
+% This is the filename of noise data.
+% Ouput decorrelation matrix and noise samples. 
+
 noise_filename = 'pre_scan_noise_speech_data.h5'; 
-%flag.concatenate=0;
-%[knoise,info] = read_h5_data(noise_filename,flag);
-%[dmtx] = process_noise_spectrum(knoise);
 [noise,dmtx]= process_noise_ismrmrd_data(noise_filename);
 
-% Estimate coils sensitivities from center lines:
+% Estimate coils sensitivities from 26 center lines:
 [csm,cal_images] = estimate_sensitivities(kdata,26);
 
 % Apply decorrelation matrix to data and csm:
 kdata_pw = ismrm_apply_noise_decorrelation_mtx(kdata,dmtx);
 csm_pw   = ismrm_apply_noise_decorrelation_mtx(csm,dmtx);
 
-% Apply FT
+% Apply FT. 
 image_pw = ismrm_transform_kspace_to_image(kdata_pw,[1 2 3]);
 
-% SNR without coil sensitivities; eq [5] of Kellman Erratum
+% SNR map without coil sensitivities; eq [5] of Kellman Erratum
 snr_rss = sqrt(2)*sqrt((sum(abs(image_pw).^2,4)));
 
 % SNR with B1 weighting; eq [6] of Kellman Erratum
@@ -50,6 +49,3 @@ subplot(1,3,2), imshow(snr_b1(:,:,18),[]);  title('SNR B1'); colormap(jet); colo
 subplot(1,3,3), imshow(snr_b1(:,:,18) - snr_rss(:,:,18),[]);  title('Difference');colormap(jet); colorbar; 
 
 
-% Note: seems like body image has 2 coils, and body noise has 8. Also, the
-% speech imaging data shows 8 coils, but speech coil has at most 3
-% channels. 
